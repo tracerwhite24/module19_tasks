621f4f810472c4b883456aea0f4434ea01bcc17d
tracerwhite <tracerwhite@gmail.com>
2024-12-31 17:39:15 +0300

    Задача "Модели игрового магазина", с тремя таблицами

---
+++ store01/task1/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyers = models.ManyToManyField(Buyer, related_name='games')
+
+    def __str__(self):
+        return self.title
